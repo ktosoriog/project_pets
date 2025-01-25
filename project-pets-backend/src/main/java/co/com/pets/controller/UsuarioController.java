dc920d28d970b412380071901e0e0ef20ef3a8b9
Katerin Osorio <ktosoriog@gmail.com>
2025-01-24 19:00:24 -0500

    Avance Backend

--- project-pets-backend/src/main/java/co/com/pets/controller/UsuarioController.java
+++ project-pets-backend/src/main/java/co/com/pets/controller/UsuarioController.java
@@ -1,13 +1,7 @@
 package co.com.pets.controller;
 
-import java.util.List;
-
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,29 +18,9 @@ public class UsuarioController {
 		this.usuarioService = usuarioService;
 	}
 
-	@GetMapping
-	public List<UsuarioDTO> listar() {
-		return usuarioService.listarUsuarios();
-	}
-
 	@GetMapping("/{id}")
 	public UsuarioDTO buscarPorId(@PathVariable Integer id) {
 		return usuarioService.buscarPorId(id);
-	}
-
-	@PostMapping
-	public UsuarioDTO crear(@RequestBody UsuarioDTO usuarioDTO) {
-		return usuarioService.crearUsuario(usuarioDTO);
-	}
-
-	@PutMapping("/{id}")
-	public UsuarioDTO actualizar(@PathVariable Integer id, @RequestBody UsuarioDTO usuarioDTO) {
-		return usuarioService.actualizarUsuario(id, usuarioDTO);
-	}
-
-	@DeleteMapping("/{id}")
-	public void eliminar(@PathVariable Integer id) {
-		usuarioService.eliminarUsuario(id);
 	}
 
 }
--- project-pets-backend/src/main/java/co/com/pets/service/UsuarioService.java
+++ project-pets-backend/src/main/java/co/com/pets/service/UsuarioService.java
@@ -1,20 +1,10 @@
 package co.com.pets.service;
 
-import java.util.List;
-
 import co.com.pets.dto.UsuarioDTO;
 
 public interface UsuarioService {
 
 	UsuarioDTO buscarPorId(Integer idUsuario);
-
-	List<UsuarioDTO> listarUsuarios();
-
-	UsuarioDTO crearUsuario(UsuarioDTO usuarioDTO);
-
-	UsuarioDTO actualizarUsuario(Integer idUsuario, UsuarioDTO usuarioDTO);
-
-	void eliminarUsuario(Integer idUsuario);
 
 	UsuarioDTO buscarPorCorreo(String correo);
 
